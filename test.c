06ffe0892dc74945dcd32f3662a9e5b18a954415
taroandpuff <taroandpuff@gmail.com>
2023-04-16 22:04:43 +0800

    test

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//从顺序表中删除具有最小值的元素(假设唯一)并由函数返回被删元素的值。
+//空出的位置由最后一个元素填补，若顺序表为空，则显示出错信息并退出运行。
+struct test{
+    int a;
+    
+};
+int main() {
+    printf("xuHuanHuan");
+    return 0;
+}
